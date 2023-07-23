165ce08963c0a4bb8d39a01230ff4ad461972838
fengchunyu00 <140159233+fengchunyu00@users.noreply.github.com>
2023-07-23 17:29:29 +0800

    pull commit

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("hot-fix  test!");
         System.out.println("push  test!");
         System.out.println("push  test2!");
+        System.out.println("pull  test2!");
     }
 }
